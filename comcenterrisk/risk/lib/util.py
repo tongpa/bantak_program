f3fd87fa32de5751014a405877f1b9387f4d87db
tongpa <tong_pa@hotmail.com>
2017-08-19 09:27:06 +0700

    Create util.py

---
+++ comcenterrisk/risk/lib/util.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import requests,json
+import urllib
+
+
+class Util(object):
+    URL_LINE_NOTIFY = "https://notify-api.line.me/api/notify"
+    LINE_HEADERS = {
+            'Content-Type':'application/x-www-form-urlencoded'
+        }
+    MESSAGE_FIELD = {'message' : ''}
+    def __init__(self):
+        pass
+    
+    def calllinenotify(self, LINE_ACCESS_TOKEN, message = ""):      
+        self.LINE_HEADERS['Authorization'] = 'Bearer ' + LINE_ACCESS_TOKEN
+        self.MESSAGE_FIELD['message'] = message
+        
+        session = requests.Session() 
+        a=session.post(
+            url= self.URL_LINE_NOTIFY,
+            headers= self.LINE_HEADERS
+            ,data=self.MESSAGE_FIELD
+            )
+        value = json.loads(str(a.text))
+        print(  value)
+        
+        print('Response HTTP Status Code: {status_code}'.format( status_code=value.get('status')))
+
+#pip install -U requests[security]
+#sudo pip install -U requests[security]
+#yum install libffi-devel python-cffi
+#https://stackoverflow.com/questions/22252397/importerror-no-module-named-mysqldb
+
+#pip install cffi enum34 ethtool pcp perf ply pycparser pycurl pyparsing urlgrabber urwid -y
+
+
+#requests.exceptions.SSLError: HTTPSConnectionPool(host='notify-api.line.me', port=443): Max retries exceeded with url: /api/notify (Caused by SSLError(SSLError("bad handshake: Error([('SSL routines', 'SSL3_GET_SERVER_CERTIFICATE', 'certificate verify failed')],)",),))
